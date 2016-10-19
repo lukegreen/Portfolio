43bd671e0a7dbbc055bc9e73479e57b06f1634ad
Luke Green <elukuent@gmail.com>
2016-10-19 16:26:11 -0700

    working on projects, bio, contacts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
 
-  //$("#developer, #artist, #geek").addClass("fadedIn");
-  //console.log("I am faded");
-
   var waypoint1 = new Waypoint({
   element: document.getElementById('main-title'),
   handler: function(direction) {
@@ -30,13 +27,10 @@ $(document).ready(function(){
 
   var skillsWaypoint = new Waypoint ({
     element: document.getElementById('skills'),
-    handler: function(direction) {
-      if (direction === 'down') {
+    handler: function(down) {
         $('#html, #css, #javascript, #angular, #photoshop, #illustrator, #blender, #indesign').addClass("full");
-        console.log('I am full');
-      }
     },
-    offset: 590
+    offset: 800
   });
 
 })
