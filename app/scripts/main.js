dcd796962804f45379f2c64de953349b59b55063
Luke Green <elukuent@gmail.com>
2016-12-15 11:09:11 -0800

    Added portfolio wells, content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,42 +2,19 @@ $(document).ready(function(){
 
   $('.skill-box').hover(function(){
     $(this).children('h2').fadeOut(500);
-    
+
   }, function(){
     $(this).children('h2').fadeIn();
-  })
-
-  /*var waypoint1 = new Waypoint({
-  element: document.getElementById('main-title'),
-  handler: function(direction) {
-    if (direction === 'down') {
-      $('#main-title').fadeOut('slow');
- }
- else {
-   $('#main-title').fadeIn('slow');
- }
-  },
-  offset: 200
-});
-
-  var paragraphWaypoint = new Waypoint ({
-    element: document.getElementById('main-title'),
-    handler: function(direction) {
-      if (direction === 'down') {
-        $('#statement').css('visibility','visible').hide().fadeIn('slow');
-      } else {
-        $('#statement').fadeOut('slow');
-      }
-    },
-    offset: 150
   });
 
-  var skillsWaypoint = new Waypoint ({
-    element: document.getElementById('skills'),
-    handler: function(down) {
-        $('#html, #css, #javascript, #angular, #photoshop, #illustrator, #blender, #indesign').addClass("full");
-    },
-    offset: 800
-  });*/
+  $('#application-button').on('click', function(){
+    console.log('I heard a click.');
+    var imageDisplay = $('#responsive-sample').css('display');
+    if(imageDisplay != 'none') {
+      $('#responsive-sample').fadeOut(200);
+    } else {
+      $('#responsive-sample').fadeIn(200);
+    }
+  });
 
 })
