1107120f3833020a3467153d839fe5783a4193f8
Luke Green <elukuent@gmail.com>
2017-02-04 15:39:10 -0800

    Add JS to change 'active' class on menu items
    Best damn code I've ever written!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
 
+  /*Change active class for menu items*/
+
+  $('.nav li').on('click', function(){
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  /*Skill box interactivity*/
+
   $('.skill-box').hover(function(){
     $(this).children('h2').fadeOut(500);
 
   }, function(){
     $(this).children('h2').fadeIn();
   });
+
+  /*Featurette buttons*/
 
   $('#application-button').on('click', function(){
     console.log('I heard a click.');
